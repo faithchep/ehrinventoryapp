bdb20ce7f7b9759bcd0bf070a48b46105b92e1f8
Injiri <simoninjiri@gmail.com>
2021-07-24 10:24:07 +0300

    relay non-pharm stock fragments on separate page from drugs stock main page

---
+++ omod/src/main/java/org/openmrs/module/ehrinventoryapp/page/controller/ItemStockPageController.java
@@ -0,0 +1,16 @@
+package org.openmrs.module.ehrinventoryapp.page.controller;
+
+import org.openmrs.ui.framework.UiUtils;
+import org.openmrs.ui.framework.page.PageModel;
+
+public class ItemStockPageController extends MainPageController {
+
+    public static void controller(){
+
+    }
+
+    @Override
+    public String get(PageModel pageModel, UiUtils uiUtils) {
+        return super.get(pageModel, uiUtils);
+    }
+}
